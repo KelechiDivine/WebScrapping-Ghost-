056604a44fca1f925dc3c48a7b37ed28e43f7d20
Kelechi Divine <74909446+KelechiDivine@users.noreply.github.com>
2021-03-08 16:18:06 +0100

    Using python for webScrapping

---
+++ webScrappingProjects/Oxylab_Web_Scrapping.py
@@ -0,0 +1,4 @@
+from selenium import webdriver
+
+drive = webdriver.Chrome(executable_path='C:/Users/Kelechi Divine/Downloads/chromedriver_win32/chromedriver.exe')
+drive.get('https://oxylabs.io/blog')
